823053dae3fb7929806087ddce228e0d04315f42
elevatedxd-droid <elevatedxd@gmail.com>
2026-09-24 10:40:05 +0530

    Create Catalogue.ts

---
+++ providers/Netflix/Catalogue.ts
@@ -0,0 +1,8 @@
+export const catalog = [
+  { title: "Popular Movies", filter: "/category/popular-movies" },
+  { title: "Latest TV Shows", filter: "/category/latest-tv-shows" },
+];
+export const genres = [
+  { title: "Action", filter: "/genre/action" },
+  { title: "Drama", filter: "/genre/drama" },
+];
